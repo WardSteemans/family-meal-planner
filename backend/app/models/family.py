16f73ea59b4ec8ae94c541c600d3ca5a6b7d8f31
ward <steemansward@gmail.com>
2026-02-09 22:04:04 +0100

    added family model with UUID primary

---
+++ backend/app/models/family.py
@@ -0,0 +1,27 @@
+import uuid
+from datetime import datetime
+from sqlalchemy import Column, String
+
+
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .database import Base
+
+class Family(Base):
+    __tablename__ = "families"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True,
+        default=uuid.uuid4,
+        index=True
+    )
+    name: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    #relationship to access family.users
+    users: Mapped[list["User"]] = mapped_column(relationship="User", back_populates="family")
+
+    def __repr__(self):
+        return f"Family(id={self.id}, name={self.name})"
